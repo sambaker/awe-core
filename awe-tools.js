218fb6a511785d1c3dbcf22174468d6d250a8884
Shyam Habarakada <shyam.habarakada@gmail.com>
2012-04-05 14:20:30 -0700

    update enableCTrace logic

--- awe-tools.js
+++ awe-tools.js
@@ -6,9 +6,28 @@
  */
 (function(Awe, global, document, undefined) {
 
-  var xyTrace = function(evt) {
-    console.log("point:( " + evt.x + ", " + evt.y + " ) of " + evt.currentTarget.id);
+  /*
+   * method: Schedule a timeout that prints out the mouse coordinates only after 
+   * mousemove has stopped
+   */
+  var onmousemove = function(evt) {
+    clearTimeout(mousemoveTimer);
+    mousemoveEvent = evt;
+    mousemoveElementId = evt.currentTarget.id;
+    mousemoveTimer = setTimeout(onmousestop, 250);
   }
+  
+  var onmouseout = function(evt) {
+    clearTimeout(mousemoveTimer);
+  }
+
+  var onmousestop = function() {
+    console.log("point:( " + mousemoveEvent.x + ", " + mousemoveEvent.y + " ) of " + mousemoveElementId);
+  }
+
+  var mousemoveEvent;
+  var mousemoveElementId;
+  var mousemoveTimer;
   
   /* 
    * method: Awe.enableCTrace
@@ -18,7 +37,8 @@
    */
   Awe.enableCTrace = function(elementId) {
     var e = document.getElementById(elementId);
-    e.addEventListener("mousemove", xyTrace);
+    e.addEventListener("mousemove", onmousemove);
+    e.addEventListener("mouseout", onmouseout);
   }
 
   /* 
@@ -28,7 +48,8 @@
    */
   Awe.disableCTrace = function(elementId) {
     var e = document.getElementById(elementId);
-    e.removeEventListener("mousemove", xyTrace);
+    e.removeEventListener("mousemove", onmousemove);
+    e.removeEventListener("mouseout", onmouseout);
   }
   
 })(Awe, this, document);
