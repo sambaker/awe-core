9f0d696acfb25d41abd8fb7aa55fd2420708ccea
Sam Baker <sam@ag-1139-mac.local>
2012-07-31 15:59:40 -0700

    Add Awe.injectJS

--- awe-dom.js
+++ awe-dom.js
@@ -62,6 +62,41 @@
     return el;
   }
 
+  Awe.injectJS = function(jsUrl, jsOnload, addRandomParam) {
+      var scriptTag = null;
+
+      var callbackName = "jsonp" + Awe.getGuidNumeric();
+
+      _headTag = window._headTag || document.getElementsByTagName("HEAD")[0];
+
+      window[callbackName] = function(data) {
+          if (jsOnload)
+          {
+             jsOnload(data);
+          }
+          _headTag.removeChild(scriptTag);
+          window[callbackName] = null;
+      }
+
+      if (jsUrl.indexOf('?') < 0) {
+          jsUrl += '?';
+      } else {
+          jsUrl += '&';
+      }
+      jsUrl += "callback="+callbackName;
+
+      scriptTag = document.createElement("SCRIPT");
+      scriptTag.type = 'text/javascript';
+
+      if ( addRandomParam )
+      {
+          jsUrl += "&randomParam=" + Math.round(Math.random() * 100000);
+      }
+
+      scriptTag.src = jsUrl;
+      _headTag.appendChild(scriptTag);
+  }
+
   var requestAnimationFrameShim = (function() {
     return  global.requestAnimationFrame       ||
             global.webkitRequestAnimationFrame || 
