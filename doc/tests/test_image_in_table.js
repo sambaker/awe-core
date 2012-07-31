7b77231e61014b16fd9eada32cf669af2a3fde39
Sam Baker <sam@ag-1139-mac.local>
2012-07-31 15:58:58 -0700

    Switch to docco for documentation

--- doc/tests/test_image_in_table.js
+++
@@ -1,9 +0,0 @@
-// = test_image_in_table.js = 
-
-// ** {{{ Test image in table }}} **
-// |= Header 1 |= Header 2 |
-// | [[http://www.ibm.com|Big Corp]] | some text in col 2 |
-// | {{http://www.wikicreole.org/attach/LeftMenu/viki.png|Creole 1.0}} | more text in col 2 |
-
-function dummy() {
-}
